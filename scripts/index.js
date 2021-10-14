afd4635467af063509dd3e4e7fca8eb3e6f4fa81
Elana Tollu <elana.tollu@yandex.ru>
2021-10-14 11:33:03 +0300

    handle signUp

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,10 @@
 const buttonMenu = document.querySelector('.button-menu');
 const popupMenu = document.querySelector('.popup');
-
+const signUpForm = document.querySelector('.form');
 
 function init () {
   buttonMenu.addEventListener('click', toggleMenu);  // Прицепить listener
-
-
+  signUpForm.addEventListener('submit', signUp);
 }
 
 function toggleMenu () {
@@ -13,6 +12,11 @@ function toggleMenu () {
   popupMenu.classList.toggle('popup_open');
 }
 
-
+function signUp(submitEvent) {
+  submitEvent.preventDefault();  // Не отправлять форму на сервер и не перезагружать страницу
+  const email = signUpForm.elements['email'].value;
+  signUpForm.reset();
+  alert(`Уважаемый ${email}, Ваша заявка принята!`);
+}
 
 init();
